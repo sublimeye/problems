47f3f7a48eb42af46caf73dcac34919e8e1881c4
Roman Morozov <roman.morozov@bsci.com>
2019-03-10 10:28:16 -0700

    added a few more

---
+++ src/lc215.kthLargest.test.ts
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(a: number[], b: number[]): number[] {
+  // 1. build two dictionaries and iterate O(n), O(~n) memory
+  // 2. if both are sorted: solved with 2 pointers: O(n logn), (1) memory (not including returning On array)
+  // if less(a, b) -> move a pointer
+  // if less(b, a) -> move b pointer
+  // if equals - move both pointers (until !==) and push to an array
+  // 3. if stored on disk and can't load in memory:
+  // interesting question :-)
+  // find out how many el can fit in memory, scan M elements
+  // and search for intersection in the second array (how to keep track of already "scanned?")
+}
+
+test('reverse string', () => {
+  expect(intersect([1, 2, 2, 1], [2, 2])).toEqual([2, 2])
+  //expect(intersect([4, 9, 5], [9, 4, 9, 8, 4])).toEqual([4, 9])
+})
--- src/temp.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        return 123
