0f33e182836b1ae81b807f910ddae73357200e0c
Roman Morozov <sublimeye.ua@gmail.com>
2019-04-22 22:47:16 -0700

    lc146 updated solution

--- src/lc146.lru-cache.test.ts
+++ src/lc146.lru-cache.test.ts
@@ -1,33 +1,45 @@
 // https://leetcode.com/problems/lru-cache/
 
+/**
+ * LRU cache.
+ * https://www.cs.princeton.edu/courses/archive/fall18/cos226/exams/mid-f12-sol.pdf
+(a) We use two data structures:
+• A doubly-linked list containing the N keys in the cache, with the most-recently cached key
+ at the front and the least-recently cached key at the end.
+LRU cache data structure
+• A symbol table containing the N keys in the cache, where the value is a reference to the node
+ in the doubly-linked list containing that key.
+To achieve the performance requirement, we use a linear probing hash table of capacity 2N for
+the symbol table. (A separate-chaining hash table would also work.)
+
+inCache(Key key):
+• Use the symbol table to determine whether the key is in the cache.
+(c) cache(Key key):
+• If the key is already in the cache, move the corresponding linked list node from its current position to the front of the list.
+• If the key is not already in the cache
+– remove the last node from the linked list (and remove the corresponding key
+from the symbol table)
+– add a new node to the front of the linked list with the given key (and add a
+corresponding entry to the symbol table)
+95
+ */
+
 class DLinkedNode {
   key?: number
   value?: number
   prev: DLinkedNode | null = null
   next: DLinkedNode | null = null
-
-  constructor(key?: number, value?: number) {
-    if (typeof key === 'number') {
-      this.key = key
-    }
-
-    if (typeof value === 'number') {
-      this.value = value
-    }
-  }
 }
 
 class LRUCache {
-  private cache: { [key: number]: DLinkedNode } = {}
-  private size: number = 0
+  private cache: Map<number, DLinkedNode> = new Map()
+  size: number = 0
   private capacity: number
-  private head: DLinkedNode
-  private tail: DLinkedNode
+  private head: DLinkedNode = new DLinkedNode()
+  private tail: DLinkedNode = new DLinkedNode()
 
   constructor(capacity: number) {
     this.capacity = capacity
-    this.head = new DLinkedNode()
-    this.tail = new DLinkedNode()
     this.head.next = this.tail
     this.tail.prev = this.head
   }
@@ -67,7 +79,8 @@ class LRUCache {
   }
 
   get(key: number): number | undefined {
-    const node = this.cache[key]
+    const node = this.cache.get(key)
+
     if (!node) return -1
 
     // move to head = update priority
@@ -77,22 +90,24 @@ class LRUCache {
   }
 
   put(key: number, value: number) {
-    const node = this.cache[key]
+    const node = this.cache.get(key)
     if (node) {
       // overwrite value
       node.value = value
       this.deleteNode(node)
       this.addToHead(node)
     } else {
-      const node = new DLinkedNode(key, value)
-      this.cache[key] = node
+      const node = new DLinkedNode()
+      node.key = key
+      node.value = value
+      this.cache.set(key, node)
       this.addToHead(node)
       this.size++
 
       if (this.size > this.capacity) {
         const preTail = this.popTail()
         if (preTail && preTail.key !== undefined) {
-          delete this.cache[preTail.key]
+          this.cache.delete(preTail.key)
         }
         this.size--
       }
@@ -116,6 +131,7 @@ describe('LRU Cache', () => {
   test('should remove older items', () => {
     const c = new LRUCache(3)
     c.put(1, 10)
+    expect(c.size).toEqual(1)
     c.put(2, 20)
     c.put(3, 30)
     c.put(4, 40)
@@ -123,6 +139,7 @@ describe('LRU Cache', () => {
     expect(c.get(1)).toEqual(-1)
     expect(c.get(2)).toEqual(-1)
     expect(c.get(3)).toEqual(30)
+    expect(c.size).toEqual(3)
   })
 })
 
@@ -131,28 +148,4 @@ describe('LRU Cache', () => {
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
  * obj.put(key,value)
- */
-
-/**
- * LRU cache.
- * https://www.cs.princeton.edu/courses/archive/fall18/cos226/exams/mid-f12-sol.pdf
-(a) We use two data structures:
-• A doubly-linked list containing the N keys in the cache, with the most-recently cached key
- at the front and the least-recently cached key at the end.
-LRU cache data structure
-• A symbol table containing the N keys in the cache, where the value is a reference to the node
- in the doubly-linked list containing that key.
-To achieve the performance requirement, we use a linear probing hash table of capacity 2N for 
-the symbol table. (A separate-chaining hash table would also work.)
-
-inCache(Key key):
-• Use the symbol table to determine whether the key is in the cache.
-(c) cache(Key key):
-• If the key is already in the cache, move the corresponding linked list node from its current position to the front of the list.
-• If the key is not already in the cache
-– remove the last node from the linked list (and remove the corresponding key
-from the symbol table)
-– add a new node to the front of the linked list with the given key (and add a
-corresponding entry to the symbol table)
-95
  */
