b15fcb3c294a2bd88406b73f74c97615a5b41b3f
Roman Morozov <roman.morozov@bsci.com>
2019-06-21 10:15:58 -0700

    trees

--- src/lc589.n-ary-tree-preorder-traversal.ts
+++ src/lc589.n-ary-tree-preorder-traversal.ts
@@ -1,11 +1,58 @@
+// Given an n-ary tree, return the preorder traversal of its nodes' values.
+// Recursive solution is trivial, could you do it iteratively?
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
  */
-var subarraySum = function(nums: number[], k: number): number {}
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var preorder2 = function(root) {
+  if (!root) return []
+  let ch = []
+  if (root.children) {
+    for (let c of root.children) {
+      ch = ch.concat(preorder(c))
+    }
+  }
+  return [root.val].concat(ch)
+}
 
-test.skip('selection sort', () => {
-  expect(subarraySum([1, 1, 1], 2)).toEqual(2)
-  expect(subarraySum([1, 2, 1, 2, 1], 3)).toEqual(4)
-})
+var preorder3 = function(root) {
+  const result = []
+  function traverse(node) {
+    if (!node) return null
+    result.push(node.val)
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i++) {
+        traverse(node.children[i])
+      }
+    }
+  }
+  traverse(root)
+  return result
+}
+
+var preorder = function(root) {
+  const result = []
+  const q = []
+
+  q.push(root)
+
+  while (q.length) {
+    const n = q.pop()
+    if (!n) continue
+    result.push(n.val)
+    if (n.children) {
+      for (let i = n.children.length - 1; i >= 0; i--) {
+        q.push(n.children[i])
+      }
+    }
+  }
+
+  return result
+}
--- src/lc965.univalued-binary-tree.ts
+++ src/lc965.univalued-binary-tree.ts
@@ -1,45 +1,21 @@
 /**
- * @param {string[]} emails
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
  */
-var numUniqueEmails = function(emails: string[]): number {
-  // build a hash of clear emails
-  // return number of keys
-  const d: { [s: string]: string } = {}
-  // ways to clear local name
-  // 1. regexp: easy~ish to read, might be inefficient
-  // 2. manual string modification - iterate until @ reached
-  for (let email of emails) {
-    let entry: string[] = []
-    let skip = false
-    for (let c = 0; c < email.length; c++) {
-      if (email[c] === '.' || (skip && email[c] !== '@')) {
-        continue
-      } else if (email[c] === '+') {
-        skip = true
-        continue
-      }
-
-      if (email[c] === '@') {
-        entry.push(email.substring(c))
-        break
-      } else {
-        entry.push(email[c])
-      }
-    }
-    const e = entry.join('')
-    d[e] = e
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+  if (!root) return false
+  const val = root.val
+  function traverse(root, val) {
+    if (!root) return true
+    if (root.val !== val) return false
+    return traverse(root.left, val) && traverse(root.right, val)
   }
-
-  return Object.keys(d).length
+  return traverse(root, val)
 }
-
-test('unique email address', () => {
-  expect(
-    numUniqueEmails([
-      'test.email+alex@leetcode.com',
-      'test.e.mail+bob.cathy@leetcode.com',
-      'testemail+david@lee.tcode.com',
-    ]),
-  ).toEqual(2)
-})
