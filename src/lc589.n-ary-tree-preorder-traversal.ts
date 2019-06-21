e1602b38b9592b7b844587fde56c906c8f6b05bd
Roman Morozov <roman.morozov@bsci.com>
2019-06-21 10:15:50 -0700

    trees

---
+++ src/lc589.n-ary-tree-preorder-traversal.ts
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums: number[], k: number): number {}
+
+test.skip('selection sort', () => {
+  expect(subarraySum([1, 1, 1], 2)).toEqual(2)
+  expect(subarraySum([1, 2, 1, 2, 1], 3)).toEqual(4)
+})
